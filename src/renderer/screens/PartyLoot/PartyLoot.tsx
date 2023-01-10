cceec97896372042d579348f802a20d08d668dfb
Memo Beltran <bego@codiks.com>
2023-01-10 17:42:15 -0600

    prettify share loot

--- src/renderer/screens/PartyLoot/PartyLoot.tsx
+++ src/renderer/screens/PartyLoot/PartyLoot.tsx
@@ -1,27 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import dummyPartyLoot from '../../../__data__/dummyPartyLoot.txt';
 import CalculateLoot from '../../../tools/party-loot';
 import './PartyLoot.css';
 
 const PartyLoot = () => {
   const [text, setText] = useState(dummyPartyLoot);
+  const [outputText, setOutputText] = useState('');
+
+  const recalculate = useCallback(() => {
+    const result = CalculateLoot(text);
+    setOutputText(result);
+  }, [text]);
+
   useEffect(() => {
-    CalculateLoot();
-  }, []);
+    recalculate();
+  }, [recalculate]);
+
+  const updateInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+    recalculate();
+  };
   return (
     <div>
-      <h1>Party Loot Share</h1>
+      <h1 className="title">Party Loot Share</h1>
       <div className="mainContent">
-        <span>Calculate your share in the hunt.</span>
+        <span>Calculate your cut in the hunt.</span>
         <div>
           <textarea
             id="party-loot-input"
             className="party-loot-textarea"
-            defaultValue={text}
+            value={text}
+            onChange={updateInput}
           />
         </div>
         <div>
-          <span id="party-loot-output" />
+          <div
+            id="party-loot-output"
+            className="party-loot-output"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: outputText }}
+          />
         </div>
       </div>
     </div>
